ada3741da2d204a4339ea0bbf05404f9a6c2425c
HoaNe <146044211+HoaNeee@users.noreply.github.com>
2025-08-25 10:36:20 +0700

    fix request 2

--- src/app/api/chat/generate-content-with-ai/route.ts
+++ src/app/api/chat/generate-content-with-ai/route.ts
@@ -1,16 +1,17 @@
 import connectDB from "@/lib/db/connect";
 import { getMessageFromAI } from "@/lib/db/queries";
-import { errorHandler, MyRequest } from "@/lib/errors";
-import { NextResponse } from "next/server";
+import { errorHandler } from "@/lib/errors";
+import { fakeMiddleware } from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = errorHandler(async (req: MyRequest, { params }) => {
+export const POST = errorHandler(async (req: NextRequest, { params }) => {
   await connectDB();
 
   const { content } = await req.json();
 
   const { id } = await params;
 
-  const userId = req.userId;
+  const userId = fakeMiddleware(req);
 
   const data = await getMessageFromAI(id, userId, content);
 
